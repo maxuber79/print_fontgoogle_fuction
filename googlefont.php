81a900d5e96a8c6014cd4cbcc1873c81f587876a
cmunozm <cmunozm@webmain.cl>
2022-01-18 12:01:34 -0300

    Mejora del archivo y function

--- googlefont.php
+++ googlefont.php
@@ -1,10 +1,17 @@
 <?php
+
+if ( ! defined( 'THEME_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( 'THEME_VERSION', '1.0.0' );
+}
+
+
 /**
  * Register style sheet.
  */
 function google_fonts() {
-    wp_register_style( 'Ubuntu', '//fonts.googleapis.com/css?family=Ubuntu:400,400i,700' );
-    wp_enqueue_style( 'Ubuntu' );
+    wp_register_style( 'name_font', 'URL googleapifont', array(), THEME_VERSION );
+    wp_enqueue_style( 'name_font' );
 }
 // Register style sheet.
 add_action( 'wp_print_styles', 'google_fonts' );
